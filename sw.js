8a7319176a9a6838f6384fbd8fad22f32b16ef6e
Sajad00200 <sajad.star13851385@gmail.com>
2026-09-05 18:42:30 +0330

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='workbook-app-v2';const ASSETS=['./','./index.html','./manifest.json','./sw.js','./icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
